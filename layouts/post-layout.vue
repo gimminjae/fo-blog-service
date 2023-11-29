8d142cd5c6726fb70ca19b68f12fd5d020961f83
gimminjae <min356812@naver.com>
2023-11-30 00:13:44 +0900

    FEAT: post write page 생성 및 커스텀 layout 적용

---
+++ layouts/post-layout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+      <LayoutHeader />
+      <NuxtPage class="" />
+  </div>
+</template>
+<script setup>
+import 'vue3-toastify/dist/index.css'
+
+</script>
+<style scoped>
+
+</style>
---
+++ pages/post/write.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      write post page
+  </div>
+</template>
+<script setup>
+setPageLayout('post-layout')
+// export default {
+//     layout: 'post-layout',
+//     // 또는
+//     // layout(context) {
+//     //     layout 'newLayout'
+//     // }
+// }
+</script>
+<style scoped>
+</style>
