0ab1ff69cafb60d6adfa28e52472cdbcc9cfc60e
gimminjae <min356812@naver.com>
2023-12-05 19:01:18 +0900

    my page

--- pages/member/my-page.vue
+++ pages/member/my-page.vue
@@ -1,12 +1,64 @@
 <template>
   <div>
-      mypage
-
-      loginedMember:
-      {{ loginedMember }}
+      <div class="text-center">
+          <img class="rounded-circle" alt="avatar1" src="https://mdbcdn.b-cdn.net/img/new/avatars/9.webp" />
+          <div>
+              <h1>{{ loginedMember.nickname }}</h1>
+              <h4>Since {{ loginedMember.createDateTime }}</h4>
+          </div>
+      </div>
       <hr>
-      posts:
-      {{ posts }}
+      <div>
+          <div class="text-center mb-3">
+              <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+                  <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
+                  <label @click="changePostView('notTmp')" class="btn btn-outline-primary" for="btnradio1">글</label>
+
+                  <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+                  <label @click="changePostView('tmp')" class="btn btn-outline-primary" for="btnradio3">임시 글</label>
+              </div>
+          </div>
+          <div v-if="postsViewMode === 'notTmp'" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              <div class="col" v-for="post in notTmpPosts">
+                  <div class="card shadow-sm">
+                      <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">صورة مصغرة</text></svg>
+                      <div class="card-body">
+                          <p class="card-text">{{ post.title }}</p>
+                          <div class="d-flex justify-content-between align-items-center">
+                              <div class="btn-group">
+                                  <button type="button" class="btn btn-sm btn-outline-secondary">수정</button>
+                                  <button @click="removePost(post.postId)" type="button" class="btn btn-sm btn-outline-secondary">삭제</button>
+                              </div>
+                              <div>
+                                  <div class="text-body-secondary">{{ post.createDateTime }}</div>
+                                  <div class="text-body-secondary">{{ post.updateDateTime }}</div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div v-if="postsViewMode === 'tmp'" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              <div class="col" v-for="post in tmpPosts">
+                  <div class="card shadow-sm">
+                      <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">صورة مصغرة</text></svg>
+                      <div class="card-body">
+                          <p class="card-text">{{ post.title }}</p>
+                          <div class="d-flex justify-content-between align-items-center">
+                              <div class="btn-group">
+                                  <button type="button" class="btn btn-sm btn-outline-secondary">수정</button>
+                                  <button @click="removePost(post.postId)" type="button" class="btn btn-sm btn-outline-secondary">삭제</button>
+                              </div>
+                              <div>
+                                  <div class="text-body-secondary">{{ post.createDateTime }}</div>
+                                  <div class="text-body-secondary">{{ post.updateDateTime }}</div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
   </div>
 </template>
 <script setup>
@@ -14,12 +66,30 @@ import { ref } from "vue";
 import {useMainStore} from "~/store/index";
 import post from "~/composables/post"
 
-const posts = ref([])
+const postsViewMode = ref("notTmp")
+
+const notTmpPosts = ref([])
+const tmpPosts = ref([])
 const loginedMember = ref({})
 onMounted(async () => {
-    loginedMember.value = useMainStore().getMember()
-    posts.value = await post.getByMember(loginedMember.value.nickname)
+    loadPageInfo()
 })
+async function loadPageInfo() {
+    loginedMember.value = useMainStore().getMember()
+    const postsData = await post.getByMember(loginedMember.value.nickname)
+    notTmpPosts.value = postsData.notTmp
+    tmpPosts.value = postsData.tmp
+}
+function changePostView(mode) {
+    postsViewMode.value = mode
+}
+async function removePost(postId) {
+    if (!confirm('정말로 삭제하시겠습니까?')) {
+        return
+    }
+    await post.delete(postId)
+    loadPageInfo()
+}
 </script>
 <style scoped>
 </style>
